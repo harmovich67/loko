c447653169b84d4cec6bd9d734e20efaf0ce61ac
harmovich67 <saidd.mahmoud4@gmail.com>
2024-09-22 21:34:57 +0300

    new style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
-const xhr = new XMLHttpRequest();
+// const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos', true);
+// xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos', true);
 
-xhr.onload = function() {
-    if(xhr.status >= 200 && xhr.status < 300) {
-        const res = JSON.parse(xhr.responseText);
-        console.log(res)
-    }else{
-        console.error('Error!', xhr.status);
-    }
-}
+// xhr.onload = function() {
+//     if(xhr.status >= 200 && xhr.status < 300) {
+//         const res = JSON.parse(xhr.responseText);
+//         console.log(res)
+//     }else{
+//         console.error('Error!', xhr.status);
+//     }
+// }
 
-xhr.onerror = function() {
-    console.error('Req Error!');
-}
+// xhr.onerror = function() {
+//     console.error('Req Error!');
+// }
 
-xhr.send();
+// xhr.send();
